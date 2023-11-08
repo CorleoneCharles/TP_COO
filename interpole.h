dd2040a0f5ff09e17c9f55b65a3a4dfe7fd0ada2
elsapainvin <150165563+elsapainvin@users.noreply.github.com>
2023-11-08 15:57:59 +0100

    Update interpole.h

--- interpole.h
+++ interpole.h
@@ -2,6 +2,8 @@
 #define INTERPOLE_H
 
 #include "nr3.h"
+// Responsabilité : Calculer la valeur interpolée d'une image à des coordonnées données
+// test : interpoler aux frontières et à l'interieur de l'image pour vérifier
 
 class interpole{
   public : 
