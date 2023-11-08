0660b3be439cc2bf94fb72ad396ba2a2feae58b5
hm456123 <150254113+hm456123@users.noreply.github.com>
2023-11-08 16:53:16 +0100

    Update regression.h

--- regression.h
+++ regression.h
@@ -5,6 +5,7 @@
 #include "Deformation.h"
   class regressionfinale{ 
 public:
-virtual regressionfinale{ NRmatrix <int>& Image1,NRmatrix <int>& Image2}//Image 1 est l'image déformées et Image2 celle non déformée
+virtual regressionfinale{ NRmatrix <int>& Image1,NRmatrix <int>& Image2, float * transformé}//Image 1 est l'image déformées, Image2, celle non déformée, transformé pointe sur un vecteur contenant 
+// les paramètres de la transformation
 
 #endif
