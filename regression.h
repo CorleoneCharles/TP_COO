8bf25e3d47bc3079265bf11c2a7ed7df3e585f57
hm456123 <150254113+hm456123@users.noreply.github.com>
2023-11-08 16:25:40 +0100

    Update regression.h

--- regression.h
+++ regression.h
@@ -1,7 +1,7 @@
 #ifndef "REGRESSION_H"
 #define "REGRESSION_H"
-#include "interpole.h"
+
 #include "similarite.h"
 #include "Deformation.h"
-  class regression
+  class regression{ Image1,Image2}//Image 1 est l'image déformées et Image2 celle non déformée
 #endif
