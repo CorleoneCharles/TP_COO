a25ab520e9b99238b6e6d5a0c3f7ec1cadb557bd
elsapainvin <150165563+elsapainvin@users.noreply.github.com>
2023-11-30 10:31:21 +0100

    Update transformation.h

--- transformation.h
+++ transformation.h
@@ -10,7 +10,5 @@ class Transformation {
 public:
   void transforme(vecDouble& param, double x, double y, double* x_res, double* y_res) const;
 };
-// vecDouble est un vecteur de doubles qui contient les paramètres de la transformation. Il est défini dans nr3.h
-// x et y sont les coordonnées à transformer
-// les pointeurs sur x_res et y_res permettent de stocker le resultat de la transformation
+
 #endif
