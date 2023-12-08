2b48e9fe586e368edb987219ea8783313843bfeb
hm456123 <150254113+hm456123@users.noreply.github.com>
2023-12-08 13:55:26 +0100

    Update transformation.h

--- transformation.h
+++ transformation.h
@@ -9,6 +9,12 @@
 class Transformation {
 public:
   void transforme(vecDouble& param, double x, double y, double* x_transfo, double* y_transfo) const;
+double theta=vecDouble[1];
+double tx=vecDouble[2];
+double ty=vecDouble[3];
+xtransfo=cos(theta)*x-sin(theta)*y+tx;
+ytransfo=cos(theta)*y+sin(theta)*x+ty;
+
 };
 
 #endif
