a0cd27ec2c5b2148957c0f52f02e0babca457198
hm456123 <150254113+hm456123@users.noreply.github.com>
2023-12-11 09:11:35 +0100

    Update Similarite2brouillon.h

--- Similarite2brouillon.h
+++ Similarite2brouillon.h
@@ -19,40 +19,32 @@ return 	im;
 }
 
 
-
-NRmatrix<double> divisionethistogrammematrice(const NRmatrix<double> &im2, VecDoub& in){
+VecDoub divisionethistogrammematrice(const NRmatrix<double> &im2){
 im=im2;
+VecDoub histog1(16);
 for (int i=0;i<im.nrows();i++)
  {
   for (int j=0;j<im.ncols();j++){
    im[i][j]=floor(im[î][j]/16);
    
-   in[im[i][j]]=in[im[i][j]]+1;}
+   inim[im[i][j]-1]=in[im[i][j]-1]+1;}
  }
-return in;
+return histog;
 }
 
 NRmatrix<double> obtenirp12(NRmatrix<double> &im2,NRmatrix<double> &im1, NRmatrix<double> &P12){
 im3=im2;
 im=im1;
+NRmatrix<double> P12(16,16,0)
 for (int i=0;i<im.nrows();i++)
  {
   for (int j=0;j<im.ncols();j++){
    im[i][j]=floor(im[î][j]/16);
    im3[i][j]=floor(im3[î][j]/16);
-   P12[im[i][j],im3[i][j]]=P12[im[i][j],im[i][j]]+1;
-   }NRmatrix<double> im(2,3,120);
+   P12[im[i][j]-1,im3[i][j]-1]=P12[im[i][j]-1,im[i][j]-1]+1;
+   }
  }
 return P12;
-}
-
-algorecalge(NRmatrix<double> &im){
-for (int i=0;i<im.nrows();i++)
- {
-  for (int j=0;j<im.ncols();j++){
-  im[i][j]=min(2*abs(im[i][j]-128,255);
-  }
-}
 }
 
 similarite(const NRmatrix<double> &im2,const NRmatrix<double> &im1){
