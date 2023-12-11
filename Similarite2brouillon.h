90c61223e8aeb6e0f35d22c0a6378c621094e221
hm456123 <150254113+hm456123@users.noreply.github.com>
2023-12-11 08:56:29 +0100

    Update Similarite2brouillon.h

--- Similarite2brouillon.h
+++ Similarite2brouillon.h
@@ -6,18 +6,22 @@
 
 
 
-algorecalge(NRmatrix<double> &im){
+
+NRmatrix<double> algorecalge(const NRmatrix<double> &imoriginal){
+im=imoriginal;
 for (int i=0;i<im.nrows();i++)
  {
   for (int j=0;j<im.ncols();j++){
   im[i][j]=min(2*abs(im[i][j]-128,255);
   }
 }
+return 	im;
 }
 
-divisionethistogrammematrice(NRmatrix<double> &im2, VecDoub& in){
-im=im2;
 
+
+NRmatrix<double> divisionethistogrammematrice(const NRmatrix<double> &im2, VecDoub& in){
+im=im2;
 for (int i=0;i<im.nrows();i++)
  {
   for (int j=0;j<im.ncols();j++)
@@ -25,9 +29,10 @@ for (int i=0;i<im.nrows();i++)
    
    in[im[i][j]]=in[im[i][j]]+1;
  }
+return im;
 }
 
-obtenirp12(NRmatrix<double> &im2,NRmatrix<double> &im1, NRmatrix<double> &P12){
+NRmatrix<double> obtenirp12(NRmatrix<double> &im2,NRmatrix<double> &im1, NRmatrix<double> &P12){
 im3=im2;
 im=im1;
 
@@ -38,16 +43,31 @@ for (int i=0;i<im.nrows();i++)
    im3[i][j]=floor(im3[î][j]/16);
    P12[im[i][j],im3[i][j]]=P12[im[i][j],im[i][j]]+1;
  }
+return P12;
 }
 
-similarite(NRmatrix<double> &im2,NRmatrix<double> &im1){
+algorecalge(NRmatrix<double> &im){
+for (int i=0;i<im.nrows();i++)
+ {
+  for (int j=0;j<im.ncols();j++){
+  im[i][j]=min(2*abs(im[i][j]-128,255);
+  }
+}
+}
 
+similarite(const NRmatrix<double> &im2,const NRmatrix<double> &im1){
+imrecale1=algorecalage(im1);
+imrecale2=algorecalage(im2);
+histo1=divisionethistogrammematrice(imrecale1);
+histo2=divisionethistogrammematrice(imrecale2);
+NRmatrix<double> P12(16,16,0);
+	
 int somme=0;
 for (int i=0;i<p12.nrows();i++)
  {
-  for (int j=0;j<p12.ncols();j++)
+  for (int j=0;j<p12.ncols();j++){
    somme=somme+P12[i][j]*log(P12[i][j]/(i1[i]*i2[j]));
-
+  }
  }
 return somme; 
 }
